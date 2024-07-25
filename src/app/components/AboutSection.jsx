c292cfbc47e2a2408688616ef095347f4e758146
Weeskin <pierre.sourice38@gmail.com>
2024-07-25 11:41:47 +0200

    Suppression du fichier TextReveak

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,5 +1,4 @@
 "use client";
-import TextReveal from "./magicui/text-reveal";
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
